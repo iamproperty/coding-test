892a606aa525d602429103e59315869aeab6375a
James Fenwick <james.fenwick@iamproperty.com>
2019-10-28 13:47:54 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Laravel Coding Test
+
+## Tasks
+
+1. Add a page for users to register
+2. Use http://postcodes.io/ to ensure that users submit a valid postcode
+3. Send a welcome email when a user is registered
+4. Add an artisan command to list recently registered users
---
+++ app/Notifications/Welcome.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Notifications;
+
+use App\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Notification;
+
+class Welcome extends Notification implements ShouldQueue
+{
+    use Queueable;
+
+    /**
+     * @var User
+     */
+    private $user;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct(User $user)
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['database'];
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            'name' => $this->user->name,
+        ];
+    }
+}
