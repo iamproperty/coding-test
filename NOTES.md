102bac4de387d28ddaed77554f850da9e8493de5
Andrew Willis <andrew@willishq.co.uk>
2020-07-03 20:45:29 +0100

    Added accessability tags, placeholder, labels (for screen readers)

---
+++ NOTES.md
@@ -0,0 +1,8 @@
+Frontend tasks
+==============
+Firstly, I am going to complete the task for making the component acessible without changing the markup too much.
+Accessibility could be achieved with a `select` element, but I believe you are looking for using aria labels so I have
+decided to take that approach.
+
+I have realised that I am getting a 500 error trying to access the server for data.
+I can bypass this by creating a proxy response.
--- resources/js/components/AddressLookup.vue
+++ resources/js/components/AddressLookup.vue
@@ -1,44 +1,111 @@
 <template>
-  <div>
-    <Postcode @postcode="loadAddresses($event)" />
+  <div class="container">
+    <div class="row">
+      <div class="col mb-3">
+        <label class="sr-only" for="postcode-lookup">Postcode</label>
+        <Postcode
+          @postcode="loadAddresses"
+          id="postcode-lookup"
+          class="form-control"
+          aria-placeholder="Postcode"
+          placeholder="Postcode"
+        />
+      </div>
+    </div>
 
-    <ul v-show="addresses.length > 0 && show" class="list-group">
-      <li v-for="address in addresses" class="list-group-item" @click="select(address)">{{ address.line_1 }}</li>
-    </ul>
+    <div class="row">
+      <div class="col">
+        <div role="alert" class="alert alert-danger" v-show="error" v-text="error"/>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col">
+        <label class="sr-only" id="address-lookup-label">
+          Select Address
+        </label>
+        <ul
+          id="address-lookup-list"
+          v-show="addresses.length > 0 && show"
+          class="list-group"
+          role="listbox"
+          aria-labelledby="address-lookup-label"
+          :aria-activedescendant="`address-lookup-list-item-${selectedIndex}`"
+        >
+          <li
+            v-for="({line_1}, index) in addresses"
+            :id="`address-lookup-list-item-${index}`"
+            class="list-group-item cursor-pointer"
+            @click="select(index)"
+            role="option"
+            :aria-selected="index === selectedIndex ? 'true' : 'false'"
+            v-text="line_1"
+          />
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import Postcode from './Postcode'
+  import Postcode from './Postcode'
 
-export default {
-  name: 'AddressLookup',
-  data: function () {
-    return {
-      addresses: [],
-      show: false
-    }
-  },
-  methods: {
-    loadAddresses (postcode) {
-      fetch('https://addresses.iamproperty.com/postcodes/' + postcode + '/addresses')
-      .then(r => r.json())
-      .then(j => {
-        this.show = true
-        this.addresses = j.result
-      })
+  export default {
+    name: 'AddressLookup',
+    data: function () {
+      return {
+        addresses: [],
+        show: false,
+        selectedIndex: null,
+        error: null
+      }
     },
-    select(v) {
-      this.show = false
-      this.$emit('select', v)
+    methods: {
+      async loadAddresses(postcode) {
+        try {
+          this.error = null;
+          const response = await fetch(
+            'https://addresses.iamproperty.com/postcodes/' + postcode + '/addresses'
+          );
+          this.show = true;
+          const {result} = await response.json();
+          if (this.addresses.length === 0) {
+            this.error = 'Unable to find address';
+          } else {
+            this.addresses = result;
+          }
+        } catch {
+          this.error = 'Unable to load addresses.';
+          this.addresses = [
+            {
+              line_1: '123 My Street',
+            }, {
+              line_1: '125 My Street',
+            }, {
+              line_1: '127 My Street',
+            }, {
+              line_1: '129 My Street',
+            }, {
+              line_1: '131 My Street',
+            }
+          ];
+        }
+
+      },
+      select(v) {
+        this.selectedIndex = v;
+        this.show = false
+        this.$emit('select', this.addresses[v]);
+      }
+    },
+    components: {
+      Postcode
     }
-  },
-  components: {
-    Postcode
   }
-}
 </script>
 
 <style scoped>
-
+  .list-group-item:hover {
+    background-color: #ffffbb;
+  }
 </style>
