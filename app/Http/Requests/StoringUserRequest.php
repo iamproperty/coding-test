56f39038869c95449011443c7a1d8f0d6e59f700
Aly Ragab <alyragabalex@gmail.com>
2022-01-21 23:23:53 +0200

    start to build the main dependencies for the logic

---
+++ app/Http/Requests/StoringUserRequest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace app\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use App\Rules\PostCode;
+
+/**
+ * Description of StoringUserRequest
+ *
+ * @author ali
+ */
+class StoringUserRequest extends FormRequest {
+
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize() {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules() {
+        return [
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password' => ['required', 'string', 'min:8', 'confirmed'],
+            'postcode' => ['bail', 'required', 'string', 'max:255', new PostCode],
+        ];
+    }
+
+}
