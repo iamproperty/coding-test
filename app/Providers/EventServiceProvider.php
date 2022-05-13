f811816c03412f3c0aa4ca7ae74a3e56016ad26c
Khalid Zeiter <khalidzeiter9@gmail.com>
2022-05-13 17:10:13 +0200

    implement welcome send email after user registration with queuing

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,10 +2,10 @@
 
 namespace App\Providers;
 
+use App\Listeners\SendWelcomeEmailNotification;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
-use Illuminate\Support\Facades\Event;
 
 class EventServiceProvider extends ServiceProvider
 {
@@ -16,6 +16,7 @@ class EventServiceProvider extends ServiceProvider
      */
     protected $listen = [
         Registered::class => [
+            SendWelcomeEmailNotification::class,
             SendEmailVerificationNotification::class,
         ],
     ];
---
+++ resources/views/emails/welcome-mail.blade.php
@@ -0,0 +1,8 @@
+@component('mail::message')
+# Welcome
+
+Your account has been created successfully!
+
+Thanks,<br>
+{{ config('app.name') }}
+@endcomponent
